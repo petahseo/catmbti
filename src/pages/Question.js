a073265fd4ac99a70b5357f50fb46af42c49f45d
petahseo <petahseo@gmail.com>
2022-04-26 21:18:32 +0900

    finish

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { ProgressBar, Button } from 'react-bootstrap'
 import { QuestionData } from '../asset/data/questiondata'
-import { useNavigate } from 'react-router-dom'
+import { createSearchParams, useNavigate } from 'react-router-dom'
 
 const Question = () => {
   const [questionNo, setQuestionNo] = React.useState(0)
@@ -23,8 +23,19 @@ const Question = () => {
     if (QuestionData.length !== questionNo + 1) {
       setQuestionNo(questionNo + 1)
     } else {
+      // mbti 도출
+      const mbti = newScore.reduce(
+        (acc, curr) =>
+          acc + (curr.score >= 2 ? curr.id.substring(0, 1) : curr.id.substring(1, 2)), ""
+      )
+      console.log('mbti' + mbti)
       // 결과 페이지로 이동
-      navigate('/result')
+      navigate({
+        pathname: '/result',
+        search: `?${createSearchParams({
+          mbti: mbti
+        })}`
+      })
     }
     // if (type === "EI") {
     //   // 기존 스코어에 더할 값을 계산 (기존값+배점)
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -3,20 +3,30 @@ import React from 'react'
 import styled from 'styled-components'
 import CatImage from '../asset/cat_image.jpg'
 import Button from 'react-bootstrap/Button'
-import { Navigate, useNavigate } from 'react-router-dom'
 import { ResultData } from '../asset/data/resultdata'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 const Result = () => {
+  const navigate = useNavigate()
+  const [searchParams] = useSearchParams()
+  const mbti = searchParams.get('mbti')
+  const [resultData, setResultData] = React.useState({})
+
+  React.useEffect(() => {
+    const result = ResultData.find((s) => s.best === mbti)
+    setResultData(result)
+  }, [mbti])
+  console.log(resultData)
   return (
     <Wrapper>
       <Header>예비집사 판별기</Header>
       <Contents>
         <Title>결과 보기</Title>
         <LogoImage>
-          <img src={ResultData[0].image} className="rounded-circle" width={350} height={350} alt="고양이 로고" />
+          <img src={resultData.image} className="rounded-circle" width={350} height={350} alt="고양이 로고" />
         </LogoImage>
-        <Desc className='text-primary'>예비 집사님과 찰떡궁합인 고양이는 {ResultData[0].name}입니다.</Desc>
-        <Button style={{ fontFamily: "SimKyungha" }} onClick={() => Navigate('/')}>테스트 다시하기</Button>
+        <Desc className='text-primary'>예비 집사님과 찰떡궁합인 고양이는 {resultData.name}입니다.</Desc>
+        <Button style={{ fontFamily: "SimKyungha" }} onClick={() => navigate('/')}>테스트 다시하기</Button>
       </Contents>
     </Wrapper>
   )
